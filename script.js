2992f0324419239120050e422d180b7501d98599
Wilfred0zz <48364461+Wilfred0zz@users.noreply.github.com>
2020-06-07 12:58:48 -0400

    added index and Js script

---
+++ script.js
@@ -0,0 +1,31 @@
+const ar = () => {
+
+}
+
+const ac = () => {
+
+} 
+
+const rr = () => {
+
+}
+
+const rc = () => {
+
+}
+
+const fillU = () => {
+
+}
+
+const fill = () => {
+
+}
+
+const clearAll = () => {
+
+}
+
+const selected = () => {
+    
+}
