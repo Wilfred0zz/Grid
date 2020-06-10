9055f7c5eb74d13330f40375591c6f8facc7865e
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-06-10 16:24:54 -0400

    commented parts Fixes #1

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let rows = 0;
-let cols = 0;
+let rows = 0;//counter that tracks the amount of rows
+let cols = 0;//counter that keeps tracks on the amount of columns
 
 const ar = () => {
     let table = document.getElementById("myTable");
     let rowCell = document.createElement("tr");
-    if(rows == 0 && cols == 0){
+    if(rows == 0 && cols == 0){ //checks if there are no columns or rows then it will create one and keep track of it by increasing counter.
         let colCell = document.createElement("td");
         rowCell.appendChild(colCell);
         table.appendChild(rowCell);
         rows++;
         cols++;
-    }else{
+    }else{//if there columns and are adding rows it will use the counter as a way to know how many more squares to add for each column.
        for(let i = 0; i<cols; i++){
            let colCell = document.createElement("td");
            rowCell.appendChild(colCell);
        }
        table.appendChild(rowCell)
-       rows++;
+       rows++;//increases row counter for overall amount of rows
     }
 }
 
 const ac = () => {
     let table = document.getElementById("myTable");
-    if(rows == 0 && cols == 0){
+    if(rows == 0 && cols == 0){//checks if there are no rows or columns then it will create one and keep track of it by increasing counter.
         let rowCell = document.createElement("tr");
         let colCell = document.createElement("td");
         rowCell.appendChild(colCell);
@@ -30,13 +30,13 @@ const ac = () => {
         cols++;
         rows++;
     }
-    else{
+    else{// if there are rows already existing then it will add columns depending on the number of rows.
         let rowsMade = document.getElementsByTagName("tr");
         for(let i =0; i<rows; i++){
             let colCell = document.createElement('td');
             rowsMade[i].appendChild(colCell);
         }
-        cols++;
+        cols++;//increases number of columns
     }
 } 
 
