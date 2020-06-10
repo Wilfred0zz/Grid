f47f923912494fc81dac24c950bcae59c37b82a2
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-06-10 15:47:44 -0400

    added add colum but it has slight bug

--- script.js
+++ script.js
@@ -3,32 +3,42 @@ let cols = 0;
 
 const ar = () => {
     var table = document.getElementById("myTable");
-    let rowCell = document.createElement("tr");
-    let colCell = document.createElement("td")
-    rowCell.appendChild(colCell);
-    table.appendChild(rowCell);
-    console.log(table);
-    console.log(rowCell);
-    rowCell.style.border = 'solid';
-    colCell.style.border = 'solid';
-    colCell.height = '50px';
-    colCell.width = '50px';
-    colCell.style.backgroundColor = "white";
-    rows++;
+    if(rows == 0){
+        let rowCell = document.createElement("tr");
+        let colCell = document.createElement("td");
+        rowCell.appendChild(colCell);
+        table.appendChild(rowCell);
+        rows++;
+        cols++;
+    }else{
+        let rowCell = document.createElement("tr");
+       for(let i = 0; i<cols; i++){
+           let newCol = document.createElement("td");
+           rowCell.appendChild(newCol);
+       }
+       table.appendChild(rowCell)
+       rows++;
+    }
 }
 
 const ac = () => {
     var table = document.getElementById("myTable");
-    let rowCell = document.createElement("tr");
-    let colCell = document.createElement("td")
-    colCell.appendChild(rowCell);
-    table.appendChild(colCell);
-    rowCell.style.border = 'solid';
-    colCell.style.border = 'solid';
-    colCell.height = '50px';
-    colCell.width = '50px';
-    colCell.style.backgroundColor = "white";
-    cols++;
+    if(rows == 0){
+        let rowCell = document.createElement("tr");
+        let colCell = document.createElement("td");
+        colCell.appendChild(rowCell);
+        table.appendChild(colCell);
+        cols++;
+        rows++;
+    }
+    else{
+        let rowsMade = document.getElementsByTagName("tr");
+        for(let i =0; i<rows; i++){
+            let colCell = document.createElement('td');
+            rowsMade[i].appendChild(colCell);
+        }
+        cols++;
+    }
 } 
 
 const rr = () => {
